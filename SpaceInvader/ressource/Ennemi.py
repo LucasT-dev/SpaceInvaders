6ed2d647afbbb456fa9976e1e381d9d968ee41fb
BuildTools <unconfigured@null.spigotmc.org>
2023-04-30 17:01:50 +0200

    Initial commit Ennemi.py

---
+++ SpaceInvader/ressource/Ennemi.py
@@ -0,0 +1,10 @@
+class Ennemi:
+
+    def __init__(self):
+        self.lifePoint
+        self.modele
+        self.speed
+        self.maxSpeed
+        self.acc
+        self.maxAcc
+        self.shield
