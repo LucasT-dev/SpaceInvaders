8f19ded9739f8350841214850fd4f536d69641c5
BuildTools <unconfigured@null.spigotmc.org>
2023-05-07 18:04:45 +0200

    Add vaisseau position

--- SpaceInvader/Vaisseau.py
+++ SpaceInvader/Vaisseau.py
@@ -19,9 +19,11 @@ class Vaisseau:
 
     def moveRight(self):
         self.position.x = self.position.x + 10
+        core.memory("textureV").pos = self.position
 
     def moveLeft(self):
         self.position.x = self.position.x - 10
+        core.memory("textureV").pos = self.position
 
     def show(self):
         core.Draw.rect((255, 255, 255), (self.position.x, self.position.y, 30, 40))
