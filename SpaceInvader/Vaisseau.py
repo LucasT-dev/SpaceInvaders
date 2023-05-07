36054db84c4c6280c6ade5e74f896dc38a81c1cc
BuildTools <unconfigured@null.spigotmc.org>
2023-05-07 17:45:05 +0200

    Add methode moveLeft and moveRight

--- SpaceInvader/Vaisseau.py
+++ SpaceInvader/Vaisseau.py
@@ -1,13 +1,27 @@
+from pygame import Vector2
+
+import core
+
+
 class Vaisseau:
 
-     def __init__(self, score, modele, speed, maxSpeed, acc, maxAcc, lifePoint, shottingSpeed, shield, position):
-         self.score = score
-         self.modele = modele
-         self.speed = speed
-         self.maxSpeed = maxSpeed
-         self.acc = acc
-         self.maxAcc = maxAcc
-         self.lifePoint = lifePoint
-         self.shottingSpeed = shottingSpeed
-         self.shield = shield
-         self.position = position
+    def __init__(self):
+        self.score = 0
+        self.modele = 0 # !!!
+        self.speed = 2
+        self.maxSpeed = 5
+        self.acc = 2
+        self.maxAcc = 3
+        self.lifePoint = 100
+        self.shottingSpeed = 2
+        self.shield = False
+        self.position = Vector2(500, 700)
+
+    def moveRight(self):
+        self.position.x = self.position.x + 10
+
+    def moveLeft(self):
+        self.position.x = self.position.x - 10
+
+    def show(self):
+        core.Draw.rect((255, 255, 255), (self.position.x, self.position.y, 30, 40))
