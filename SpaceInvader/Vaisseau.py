6b2550c97f3ef0220dff02987cffb527d14382a7
BuildTools <unconfigured@null.spigotmc.org>
2023-05-13 16:16:03 +0200

    remove shhow method and addPoint method

--- SpaceInvader/Vaisseau.py
+++ SpaceInvader/Vaisseau.py
@@ -7,7 +7,7 @@ class Vaisseau:
 
     def __init__(self):
         self.score = 0
-        self.modele = 0 # !!!
+        self.modele = 0  # !!!
         self.speed = 2
         self.maxSpeed = 5
         self.acc = 2
@@ -25,5 +25,8 @@ class Vaisseau:
         self.position.x = self.position.x - 7
         core.memory("textureV").pos = self.position
 
-    def show(self):
-        core.Draw.rect((255, 255, 255), (self.position.x, self.position.y, 30, 40))
+    def addPoint(self, point):
+        self.score += point
+
+    def removePoint(self, point):
+        self.score -= point
