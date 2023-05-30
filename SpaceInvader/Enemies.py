1eadd695b9718167e68d1df63b0aef064eeb19de
p2214938 <lucas.tesson@outlook.com>
2023-05-30 16:41:23 +0200

    slt

--- SpaceInvader/Enemies.py
+++ SpaceInvader/Enemies.py
@@ -9,13 +9,14 @@ from SpaceInvader.Projectile import Projectile
 class Enemies:
 
     def __init__(self, position, texture, score, speedProjectile):
-        self.lifePoint = score
+        self.score = score
         self.speedProjectile = speedProjectile
         self.modele = texture
         self.shield = False
         self.position = position
         self.color = (155, 255, 255)
         self.height = 10
+        self.projectile = []
 
     def draw(self):
 
@@ -43,3 +44,16 @@ class Enemies:
                         core.memory("Eprojectile").append(
                             Projectile(Vector2(self.position.x, self.position.y), self.speedProjectile, (255, 0, 0)))
 
+    def addProjectile(self):
+        if len(self.projectile) < 5:
+            self.projectile.append(
+                Projectile(Vector2(core.memory("vaisseau").position.x + 32, core.memory("vaisseau").position.y), 5,
+                           (0, 0, 255)))
+
+
+    def updateProjectile(self):
+
+        if len(self.projectile) < 1:
+            for i in self.projectile:
+                i.position = Vector2(i.position.x + self.speed, i.position.y + self.speed)
+                i.draw()
